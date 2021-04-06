9e3e2eee6c96205d2545af9d4d004be91ba15182
Simon Simonsson <simon.simonsson@posten.no>
2021-04-06 14:51:30 +0200

    imagewriter example compiles but not tested

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS = -DGL_GLEXT_PROTOTYPES -O3
 INCLUDES = -I./include
 BUILD = build
 LIB = lib
-LDFLAGS = -lSDL2 -lGL -L./$(LIB)
+LDFLAGS = -lGL -L./$(LIB)
 LIBRARY = $(LIB)/libmaprender.so
 
 
@@ -16,11 +16,11 @@ $(LIBRARY): maprender.c
 
 interactive: examples/interactive/main.c $(LIBRARY)
 	@mkdir -p bin
-	$(CC) $(INCLUDES) $(CFLAGS) -o bin/interactive -I./examples/interactive examples/interactive/main.c $(LDFLAGS) -lmaprender
+	$(CC) $(INCLUDES) $(CFLAGS) -o bin/interactive -I./examples/interactive examples/interactive/main.c $(LDFLAGS) -lmaprender -lSDL2
 
 imagewriter: examples/imagewriter/main.c $(LIBRARY)
 	@mkdir -p bin
-	$(CC) $(INCLUDES) $(CFLAGS) -o bin/imagewriter -I./examples/imagewriter examples/imagewriter/main.c $(LDFLAGS) -lmaprender
+	$(CC) $(INCLUDES) $(CFLAGS) -o bin/imagewriter -I./examples/imagewriter examples/imagewriter/main.c $(LDFLAGS) -lmaprender -lpng
 
 examples: interactive imagewriter
 
--- examples/imagewriter/main.c
+++ examples/imagewriter/main.c
@@ -1,15 +1,19 @@
 /**
  * This is a test application with exported nodes and ways defined in nodes.c and ways.c
  * that represent Vitoria, Esperito Santo, Brazil.
+ *
+ * Here we just render the image once and then write the output as a png file using libpng.
  */
 
 #include <maprender.h>
-#include <SDL2/SDL.h>
 #include <GL/gl.h>
 #include <GL/glext.h>
 #include "nodes.c"
 #include "ways.c"
 #include <assert.h>
+#include <png.h>
+#include <stdio.h>
+#include <string.h>
 
 #define W 1000
 #define H 1000
@@ -21,194 +25,81 @@
 #define ViewH .5
 
 
-static SDL_Window* win;
-static SDL_GLContext ctx;
-
 static GLfloat origx = OX;
 static GLfloat origy = OY;
 static GLfloat view_width = ViewW;
 static GLfloat view_height = ViewH;
 static GLuint tex;
 
-
-int init_win (int w, int h)
+int store_image (unsigned int *pixels)
 {
-	int ret = SDL_Init (SDL_INIT_VIDEO);
-	if (ret != 0)
-		return ret;
+	// output file
+	FILE *fp = fopen ("output.png", "wb");
+	if (!fp) return 1;
 
-	SDL_GL_SetAttribute (SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute (SDL_GL_DEPTH_SIZE,  24);
-	SDL_GL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, 1);
-	SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-	SDL_GL_SetAttribute (SDL_GL_CONTEXT_MINOR_VERSION, 0);
+	// setup png stuff
 
-	win = SDL_CreateWindow
-	(
-		"map viewer",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		w,
-		h,
-		SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+	png_structp pngp = png_create_write_struct (PNG_LIBPNG_VER_STRING, 0, 0, 0);
+	if (pngp == NULL) return 1;
+
+	png_infop infop = png_create_info_struct (pngp);
+	if (infop == NULL) return 1;
+
+	if (setjmp (png_jmpbuf (pngp))) return 1;
+
+	png_set_IHDR (
+		pngp,
+		infop,
+		W,
+		H,
+		8,
+		PNG_COLOR_TYPE_RGBA,
+		PNG_INTERLACE_NONE,
+		PNG_COMPRESSION_TYPE_DEFAULT,
+		PNG_FILTER_TYPE_DEFAULT
 	);
 
-	ctx = SDL_GL_CreateContext (win);
-	SDL_GL_SetSwapInterval (1);
+	// create rows
+	png_byte **rows = png_malloc (pngp, H * sizeof (png_byte *));
+	size_t rsize = W * sizeof (unsigned int);
+	for (int y = 0; y < H; y ++)
+	{
+		rows[y] = png_malloc (pngp, rsize);
+		memcpy (rows[y], pixels + y * rsize, rsize);
+	}
+
+	// write to file
+	png_init_io (pngp, fp);
+	png_set_rows (pngp, infop, rows);
+	png_write_png (pngp, infop, PNG_TRANSFORM_IDENTITY, NULL);
+
+	// free up stuff
+	for (int y = 0; y < H; y ++)
+		png_free (pngp, rows[y]);
+	png_free (pngp, rows);
+	png_destroy_write_struct (&pngp, &infop);
+	fclose (fp);
+
 	return 0;
 }
 
-void destroy_win ()
-{
-	SDL_GL_DeleteContext (ctx);
-	SDL_DestroyWindow (win);
-	SDL_Quit ();
-}
-
-void handle_events (int* done)
-{
-	float delta = 0.01;
-	static SDL_Event ev;
-	while (SDL_PollEvent (&ev))
-	{
-		if (ev.type == SDL_QUIT)
-			*done = 1;
-		else if (ev.type == SDL_KEYUP && ev.key.keysym.sym == SDLK_q)
-			*done = 1;
-
-		if (ev.type == SDL_KEYDOWN)
-		{
-			switch (ev.key.keysym.sym)
-			{
-				case SDLK_w:
-					origy += delta;
-					break;
-
-				case SDLK_s:
-					origy -= delta;
-					break;
-
-				case SDLK_a:
-					origx -= delta;
-					break;
-
-				case SDLK_d:
-					origx += delta;
-					break;
-
-				case SDLK_j:
-					view_width -= delta;
-					view_height -= delta;
-					origx += delta / 2.;
-					origy += delta / 2.;
-					break;
-
-				case SDLK_k:
-					view_width += delta;
-					view_height += delta;
-					origx -= delta / 2.;
-					origy -= delta / 2.;
-					break;
-			}
-		}
-	}
-}
-
-float texCoords[] = {
-	0.0f, 0.0f,  // lower-left corner
-	1.0f, 0.0f,  // lower-right corner
-	0.5f, 1.0f   // top-center corner
-};
-
-/*
-float vertices[] = {
-// positions          // colors           // texture coords
-0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
--0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
--0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
-};
-*/
-
-float vertices[] = {
-	0.5f,  0.5f, 0.0f,  // top right
-	0.5f, -0.5f, 0.0f,  // bottom right
-	-0.5f, -0.5f, 0.0f,  // bottom left
-	-0.5f,  0.5f, 0.0f   // top left
-};
-unsigned int indices[] = {  // note that we start from 0!
-	0, 1, 3,   // first triangle
-	1, 2, 3    // second triangle
-};
-
-const char* vertexSource = "#version 460\n"
-	"in vec2 position;\n"
-	"void main()\n"
-	"{\n"
-	"	gl_Position = vec4(position, 0.0, 1.0);\n"
-	"}\n";
-
-
-const char* fragSource = "#version 460\n"
-	"out vec4 FragColor;\n"
-	"void main()\n"
-	"{\n"
-	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-	"}\n";
-
 int main ()
 {
-	init_win (W, H);
 	assert (map_init (W, H, &tex) == 0);
+
 	map_load_nodes (vitoria_nodes, NNODES);
 	map_load_primary_ways (way_idx, way_counts, NWAYS);
 	//map_load_secondary_ways (way_idx + 5000, way_counts + 5000, 5000);
 	//map_load_tertiary_ways (way_idx + 10000, way_counts + 10000, NWAYS - 10000);
 
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexSource, NULL);
-	glCompileShader(vertexShader);
+	unsigned int *pixels = malloc (W * H * sizeof (unsigned int));
+	map_draw (origx, origy, view_width, view_height);
+	glBindTexture (GL_TEXTURE_2D, tex);
+	glGetTexImage (GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, pixels);
+	glBindTexture (GL_TEXTURE_2D, 0);
 
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragSource, NULL);
-	glCompileShader(fragmentShader);
+	if (store_image (pixels))
+		fprintf (stderr, "error storing png file\n");
 
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	int done = 0;
-	while (!done)
-	{
-		map_draw (origx, origy, view_width, view_height);
-
-		glUseProgram (shaderProgram);
-
-		glBindTexture (GL_TEXTURE_2D, tex);
-
-		// 1. then set the vertex attributes pointers
-		//glEnableVertexAttribArray(VBO);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-
-		glUseProgram (0);
-
-		//glBindTexture (GL_TEXTURE_2D, 0);
-
-		SDL_GL_SwapWindow (win);
-		handle_events (&done);
-	}
-
-	destroy_win ();
+	free (pixels);
 }
